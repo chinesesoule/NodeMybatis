ada3990c56b10f9c58873c6310a1cc21afa4f683
侯肖卜 <13909443885@163.com>
2018-07-28 19:02:57 +0800

    重构数据库错误类型

--- lib/config/Type.ts
+++ lib/config/Type.ts
@@ -4,9 +4,5 @@
  * 包括以后需要扩展的数据库
  */
 export default {
-    ER_PASSWORD_ANONYMOUS_USER:1131,//匿名密码错误
-    ER_PASSWORD_NOT_ALLOWED:1132,//
-    ER_PASSWORD_NO_MATCH:1133,//密码不匹配
-    ER_ACCESS_DENIED_NO_PASSWORD_ERROR:1698,//密码错误拒绝访问
-    ER_SET_PASSWORD_AUTH_PLUGIN:1699,//
+    
 }
